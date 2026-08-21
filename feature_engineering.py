814be437b41511d81602d481dc10d7768ab7efd0
xiaohou-88 <houhl2023@163.com>
2026-08-21 17:34:13 +0800

    Update feature_engineering.py

--- feature_engineering.py
+++ feature_engineering.py
@@ -10,6 +10,7 @@ from rdkit import DataStructs
 from rdkit.Chem.rdFingerprintGenerator import GetMorganGenerator
 from torch_geometric.data import Data, Batch
 
+
 def collate_fn(batch):
     smiles_fps, graphs, fastas, reg_labels, cls_labels, methods = zip(*batch)
     batched_smiles = torch.stack(smiles_fps)
@@ -18,8 +19,9 @@ def collate_fn(batch):
     batched_reg_labels = torch.stack(reg_labels)
     batched_cls_labels = torch.stack(cls_labels)
     batched_methods = torch.stack(methods)
-    
+
     return batched_smiles, batched_graphs, batched_fastas, batched_reg_labels, batched_cls_labels, batched_methods
+
 
 def atom_features(atom):
     symbol = atom.GetSymbol()
@@ -28,16 +30,16 @@ def atom_features(atom):
     is_hydrogen = (symbol == "H")
     is_in_ring = atom.IsInRing()
     return [
-        atom.GetAtomicNum(),           
-        degree,                        
-        int(is_aromatic),              
-        int(is_hydrogen),              
-        int(is_in_ring),               
+        atom.GetAtomicNum(),           # Atomic number
+        degree,                        # Atom degree
+        int(is_aromatic),              # Aromatic atom flag
+        int(is_hydrogen),              # Hydrogen atom flag
+        int(is_in_ring),               # Ring membership flag
     ]
- 
-def bond_features(bond):
 
-    bond_type = int(bond.GetBondTypeAsDouble())  
+
+def bond_features(bond):
+    bond_type = int(bond.GetBondTypeAsDouble())  # Single, double, or triple bond
     is_aromatic = bond.GetIsAromatic()
     is_in_ring = bond.IsInRing()
     return [
@@ -46,19 +48,20 @@ def bond_features(bond):
         int(is_in_ring),
     ]
 
+
 def smiles_to_graph(smiles):
     mol = Chem.MolFromSmiles(smiles)
     if mol is None:
-        return np.zeros((1,5), dtype=np.float32), np.zeros((2,1), dtype=np.int64), np.zeros((1,3), dtype=np.float32)
+        return np.zeros((1, 5), dtype=np.float32), np.zeros((2, 1), dtype=np.int64), np.zeros((1, 3), dtype=np.float32)
+
     node_feats = [atom_features(atom) for atom in mol.GetAtoms()]
-    
     edge_index = []
     edge_feats = []
     for bond in mol.GetBonds():
         i = bond.GetBeginAtomIdx()
         j = bond.GetEndAtomIdx()
         edge_index.append([i, j])
-        edge_index.append([j, i])  
+        edge_index.append([j, i])  # Undirected graph
         bf = bond_features(bond)
         edge_feats.append(bf)
         edge_feats.append(bf)
@@ -67,40 +70,59 @@ def smiles_to_graph(smiles):
     edge_feats = np.array(edge_feats, dtype=np.float32)
     return node_feats, edge_index, edge_feats
 
+
 def smi_tokenizer(smi):
-    """
-    Tokenize a SMILES molecule string.
-    """
-    pattern = r"(\[[^\]]+]|Br?|Cl?|N|O|S|P|F|I|b|c|n|o|s|p|\(|\)|\.|=|#|-|\+|\\\\|\/|:|~|@|\?|>|\*|\$|\%[0-9]{2}|[0-9])"
-    regex = re.compile(pattern)
-    tokens = [token for token in regex.findall(smi)]
-    # assert smi == ''.join(tokens)
+    pattern = (
+        r"(\[[^\]]+]|Br?|Cl?|N|O|S|P|F|I|"
+        r"b|c|n|o|s|p|\(|\)|\.|=|#|-|\+|"
+        r"\\|/|:|~|@|\?|>|\*|\$|\%[0-9]{2}|[0-9])"
+    )
+    tokens = re.findall(pattern, smi)
+
+    if "".join(tokens) != smi:
+        raise ValueError(
+            f"SMILES tokenization failed:\n"
+            f"Original: {smi}\n"
+            f"Rebuilt:  {''.join(tokens)}"
+        )
+
     return tokens
 
-def build_vocab(smiles_list, pad_token="PAD"):
+
+def build_vocab(smiles_list, pad_token="PAD", unk_token=None):
     vocab = set()
     for smi in smiles_list:
         vocab.update(smi_tokenizer(smi))
     vocab = [pad_token] + sorted(list(vocab))
+    if unk_token is not None and unk_token not in vocab:
+        vocab.append(unk_token)
     word2id = {word: idx for idx, word in enumerate(vocab)}
     return vocab, word2id
 
+
 class MolDataset(Dataset):
-    def __init__(self, csv_file, max_len=128):
+    def __init__(self, csv_file, max_len=128, vocab=None, word2id=None, unk_token="UNK"):
         df = pd.read_csv(csv_file)
         self.smiles = df['Standardise_SMILES'].tolist()
         self.labels = df['Standardized_Value'].values.astype(np.float32)
         self.cls_labels = df['label'].values.astype(np.int64)
         self.methods = df['method'].values.astype(np.int64)
 
-        # morgan fingerprint
         self.morgan_generator = GetMorganGenerator(radius=2, fpSize=2048)
 
         # self.fps = [self.smiles_to_fp(s) for s in self.smiles]
         self.fps = [self.generate_combined_fp(s) for s in self.smiles]
-        # 分词编码
         self.max_len = max_len
-        self.vocab, self.word2id = build_vocab(self.smiles)
+        self.unk_token = unk_token
+        if vocab is None or word2id is None:
+            self.vocab, self.word2id = build_vocab(self.smiles, unk_token=unk_token)
+        else:
+            self.vocab = list(vocab)
+            self.word2id = dict(word2id)
+            if unk_token is not None and unk_token not in self.word2id:
+                self.word2id[unk_token] = len(self.vocab)
+                self.vocab.append(unk_token)
+        self.unk_id = self.word2id.get(unk_token) if unk_token is not None else None
         self.encoded = [self.encode(smi) for smi in self.smiles]
 
     def smiles_to_fp(self, smiles):
@@ -112,20 +134,23 @@ class MolDataset(Dataset):
         DataStructs.ConvertToNumpyArray(fp, arr)
 
         return arr
-    
+
     def generate_combined_fp(self, smiles):
+        """Generate combined Morgan and MACCS fingerprint features."""
         mol = Chem.MolFromSmiles(smiles)
+        FP_DIM = 2048 + 167
+
         if mol is None:
-            return np.zeros(2048 + 2048, dtype=np.float32)
-                
+            return np.zeros(FP_DIM, dtype=np.float32)
+
         morgan_fp = self.morgan_generator.GetFingerprint(mol)
         morgan_arr = np.zeros((2048,), dtype=np.float32)
         DataStructs.ConvertToNumpyArray(morgan_fp, morgan_arr)
-            
+
         maccs_fp = MACCSkeys.GenMACCSKeys(mol)
         maccs_arr = np.zeros((167,), dtype=np.float32)
         DataStructs.ConvertToNumpyArray(maccs_fp, maccs_arr)
-        
+
         combined_fp = np.concatenate([morgan_arr, maccs_arr])
         return combined_fp
 
@@ -135,7 +160,10 @@ class MolDataset(Dataset):
             tokens += ["PAD"] * (self.max_len - len(tokens))
         else:
             tokens = tokens[:self.max_len]
-        ids = [self.word2id[token] for token in tokens]
+        if self.unk_id is None:
+            ids = [self.word2id[token] for token in tokens]
+        else:
+            ids = [self.word2id.get(token, self.unk_id) for token in tokens]
         return np.array(ids, dtype=np.int64)
 
     def __len__(self):
